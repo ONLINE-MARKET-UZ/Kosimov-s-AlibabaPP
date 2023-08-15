f8007aca9b890c7a2ba95072ac511f2d01c7a462
kosimovdev <kosimov.oyatillo@gmail.com>
2023-08-15 08:54:56 +0500

    changres

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { Button, Space } from "antd";
+
 const App = () => {
   return (
-    <div>
+    <div className="container mx-auto p-5">
       <h1 className="text-red-400">Alibaba shopping center</h1>
+      <Space
+        direction="vertical"
+        style={{
+          width: "100%",
+        }}
+      >
+        <Button type="primary" block className="bg-blue-400">
+          Primary
+        </Button>
+        <Button block>Default</Button>
+        <Button type="dashed" block>
+          Dashed
+        </Button>
+        <Button disabled block>
+          disabled
+        </Button>
+        <Button type="text" block>
+          text
+        </Button>
+        <Button type="link" block>
+          Link
+        </Button>
+      </Space>
     </div>
   );
 };
